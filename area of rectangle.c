22e0afdfd32c1e4924300ad70a9c4926244afc7a
Pranav01-max <pranavkhandekar56@gmail.com>
2025-05-04 13:19:29 +0530

    Add files via upload

---
+++ area of rectangle.c
@@ -0,0 +1,19 @@
+//name-pranav bibhishan khandekar
+//PRN-24030331378010
+//Code-Find Area and Perimeter Of Rectangle
+//Branch-VLSI
+#include<stdio.h>
+int main()
+{
+    int length,breadth,area,perimeter;
+    printf("Enter the Length Of The Rectangle");
+    scanf("%d",&length);
+    printf("Enter the Breadth Of The Rectangle");
+    scanf("%d",&breadth);
+    area=length*breadth;
+    perimeter=2*(length+breadth);
+    printf("The Area Of Rectangle is %d /n he Perimeter Of Rectangle is %d",area,perimeter);
+    return 0;
+
+    
+}
