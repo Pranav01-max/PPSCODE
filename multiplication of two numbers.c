c422310bd10c30b5d127b8189ac9071e7a995678
Pranav01-max <pranavkhandekar56@gmail.com>
2025-05-04 13:13:08 +0530

    Add files via upload

---
+++ multiplication of two numbers.c
@@ -0,0 +1,18 @@
+//name-Bhavesh Nilesh Narvekar
+//PRN-24030331378010
+//Code-Multiplication Of Two Number By Taking Input From User
+//Branch-VLSI
+
+#include<stdio.h>
+int main()
+{
+    int n1, n2, multi;
+    printf("Enter First Number");
+    scanf("%d", &n1);
+    printf("Enter Second Number");
+    scanf("%d", &n2);
+    multi = n1 * n2;
+    printf("The Multiplication of %d and %d is %d", n1, n2, multi);
+    return 0;
+
+}
