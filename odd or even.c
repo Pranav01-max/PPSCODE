bc9f992df1c31affa59c298ea817eab092a76285
Pranav01-max <pranavkhandekar56@gmail.com>
2025-05-04 13:08:32 +0530

    Add files via upload

---
+++ odd or even.c
@@ -0,0 +1,24 @@
+//name:pranav bibhishan khandekar
+//PRN-24030331378010
+//tittle-Find If number is even or odd
+//Branch-VLSI
+
+#include <stdio.h>
+
+int main() 
+{
+    int number;
+
+    printf("Enter an number: ");
+    scanf("%d", &number);
+    if(number & 1) 
+    {
+        printf("%d is odd.\n", number);
+    }
+    else
+    {
+        printf("%d is even.\n", number);
+    }
+
+    return 0;
+}
