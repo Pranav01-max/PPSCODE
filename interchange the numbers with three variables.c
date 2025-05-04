98c618eede7a68dd7a1b5d8aadf9cac86f6e2f0f
Pranav01-max <pranavkhandekar56@gmail.com>
2025-05-04 13:05:46 +0530

    Add files via upload

---
+++ interchange the numbers with three variables.c
@@ -0,0 +1,22 @@
+//name:pranab bibhishan khandekar
+//PRN-24030331378010
+//tittle-Interchange the Numbers with third variable
+//Branch-VLSI
+
+#include <stdio.h>
+
+int main() 
+{
+    int a, b, c;
+    printf("Enter value of A: ");
+    scanf("%d", &a);
+    printf("Enter value of B: ");
+    scanf("%d", &b);
+    printf("Numbers before interchange: A = %d, B = %d\n", a, b);
+    c = a;
+    a = b;
+    b = c;
+    printf("Numbers after interchange: A = %d, B = %d\n", a, b);
+    
+    return 0;
+}
