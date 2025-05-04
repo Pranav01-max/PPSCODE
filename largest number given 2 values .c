46d3a3f61ef5ff8eaed7b334e7c1bccdfbaee2a7
Pranav01-max <pranavkhandekar56@gmail.com>
2025-05-04 13:16:45 +0530

    Add files via upload

---
+++ largest number given 2 values .c
@@ -0,0 +1,29 @@
+//name-pranav bibhishan khandekar 
+//PRN-24030331378010
+//Code-Largest Number from 2 given values
+//Branch-VLSI
+
+int main() 
+{
+    int a, b, c;
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    printf("Enter second number: ");
+    scanf("%d", &b);
+    printf("Enter third number: ");
+    scanf("%d", &c);
+    if(a >= b && a >= c) 
+    {
+        printf("%d is the largest\n", a);
+    }
+    else if(b >= a && b >= c) 
+    {
+        printf("%d is the largest\n", b);
+    }
+    else 
+    {
+        printf("%d is the largest\n", c);
+    }
+    
+    return 0;
+}
