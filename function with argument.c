6589d57433d671a074f6a2d66288b3a455331863
Pranav01-max <pranavkhandekar56@gmail.com>
2025-06-13 15:38:29 +0530

    Add files via upload

---
+++ function with argument.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int addition(a,b)
+{
+    int c;
+    c=a+b;
+    printf("Addition is %d",c);
+}
+int main()
+{
+    int n1,n2;
+    printf("Enter First Number \t");
+    scanf("%d",&n1);
+    printf("Enter Second Number \t");
+    scanf("%d",&n2);
+    addition(n1,n2);
+    return 0;
+}
