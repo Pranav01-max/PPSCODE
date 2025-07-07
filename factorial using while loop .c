8f3a0154a9b6920160ba7310f94bbfdb494e6ab2
Pranav01-max <pranavkhandekar56@gmail.com>
2025-07-07 23:54:23 +0530

    Add files via upload

---
+++ factorial using while loop .c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main() 
+{
+    int num;
+    long long factorial = 1;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+    if (num < 0) 
+    {
+        printf("Error! Factorial of a negative number doesn't exist.");
+    } else 
+    {
+        int i = 1;
+        while (i <= num) 
+        {
+            factorial *= i;
+            i++;
+        }
+        printf("Factorial of %d = %lld", num, factorial);
+    }
+
+    return 0;
+}
